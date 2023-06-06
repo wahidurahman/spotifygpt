16c48075e0c85c22daf935a5b666a494234788f8
Wahidur Rahman <rahwahidur@gmail.com>
2023-06-05 22:17:26 -0700

    Create listennow.js

---
+++ spotify/listennow.js
@@ -0,0 +1,12 @@
+const playlistId = '0eV3ocud0cqiEFs1hdoMnJ';
+
+<iframe
+  title="Spotify Embed: Recommendation Playlist "
+  src={`https://open.spotify.com/embed/playlist/0eV3ocud0cqiEFs1hdoMnJ?utm_source=generator&theme=0`}
+  width="100%"
+  height="100%"
+  style={{ minHeight: '360px' }}
+  frameBorder="0"
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/>
